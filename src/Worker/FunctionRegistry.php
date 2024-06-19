5a87df09366135f5efd7445720f0d6bf89d6e428
Keith Maika <keithm@aoeex.com>
2024-06-19 15:03:00 -0400

    Add logging.

--- src/Worker/FunctionRegistry.php
+++ src/Worker/FunctionRegistry.php
@@ -7,8 +7,13 @@ use Kicken\Gearman\Events\FunctionUnregistered;
 use Kicken\Gearman\Exception\LostConnectionException;
 use Kicken\Gearman\Exception\NoRegisteredFunctionException;
 use Kicken\Gearman\ServiceContainer;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LogLevel;
 
-class FunctionRegistry implements \Countable {
+class FunctionRegistry implements \Countable, LoggerAwareInterface {
+    use LoggerAwareTrait;
+
     private ServiceContainer $services;
 
     /** @var WorkerFunction[] */
@@ -25,11 +30,17 @@ class FunctionRegistry implements \Countable {
     public function register(WorkerFunction $fn){
         $this->functionList[$this->normalize($fn->name)] = $fn;
         $this->services->eventDispatcher->dispatch(new FunctionRegistered($fn->name));
+        $this->log(LogLevel::INFO, 'Registering function', [
+            'function' => $fn
+        ]);
     }
 
     public function unregister(WorkerFunction $fn){
         unset($this->functionList[$this->normalize($fn->name)]);
         $this->services->eventDispatcher->dispatch(new FunctionUnregistered($fn->name));
+        $this->log(LogLevel::INFO, 'Removing registered function', [
+            'function' => $fn
+        ]);
     }
 
     public function isRegistered(string $fn) : bool{
@@ -43,21 +54,38 @@ class FunctionRegistry implements \Countable {
         }
 
         try {
+            $this->log(LogLevel::INFO, 'Running job', [
+                'handle' => $job->getJobHandle(),
+                'function' => $job->getFunction()
+            ]);
             $fn = $this->functionList[$normalizedFn];
             $result = call_user_func($fn->callback, $job);
             if ($result === false){
+                $this->log(LogLevel::WARNING, 'Job returned false, sending job fail event.');
                 $job->sendFail();
             } else {
+                $this->log(LogLevel::INFO, 'Job completed successfully.');
                 $job->sendComplete((string)$result);
             }
         } catch (LostConnectionException $e){
+            $this->log(LogLevel::ERROR, 'Lost connection to sever while running job.');
             throw $e;
         } catch (\Exception $e){
+            $this->log(LogLevel::ERROR, 'Encountered exception while running job.', [
+                'exception' => get_class($e),
+                'message' => $e->getMessage(),
+            ]);
             $job->sendException(get_class($e) . ': ' . $e->getMessage());
         }
     }
 
     private function normalize(string $name) : string{
         return strtolower(trim($name));
+    }
+
+    private function log(string $level, string $message, array $params = []) : void{
+        if ($this->logger){
+            $this->logger->log($level, $message, $params);
+        }
     }
 }
